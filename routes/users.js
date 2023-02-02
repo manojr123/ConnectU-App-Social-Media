e8c784068aa9ccb07c13f5d57edab9d587890431
manojr123 <manojraghavan2008@yahoo.co.in>
2023-02-02 19:36:05 +0530

    added another controller for login

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users_controller');
+
+router.get('/profile', usersController.profile);
+router.get('/login', usersController.login);
+
+module.exports = router; 
