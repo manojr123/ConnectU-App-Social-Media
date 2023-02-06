4edb6aea08cae5b04d61909a801ec0f26695f6d4
manojr123 <manojraghavan2008@yahoo.co.in>
2023-02-06 17:40:03 +0530

    Linked MongoDB using Mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "Error connecting to mongoDB"));
+
+db.once('open', function() {
+    console.log('Connected to the database :: MongoDB');
+});
+
+module.exports = db;
