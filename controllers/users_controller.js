03382a777aa3992ea7f666f619f0c834391ca7df
manojr123 <manojraghavan2008@yahoo.co.in>
2023-02-06 17:22:27 +0530

    Setup static files access for Layouts and Pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,14 @@
 module.exports.profile = function(req,res) {
-    return res.end('<h1> User Profile </h1> ');
+    //return res.end('<h1> User Profile </h1> ');
+    return res.render('user-profile', {
+        title : "User Profile"
+    });
+
 }
 module.exports.login = function(req,res) {
-    return res.end('<h1> Login </h1> ');
+    //return res.end('<h1> Login </h1> ');
+    return res.render('user-profile', {
+        title : "User Login"
+    });
+
 }
