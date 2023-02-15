228de1db82eb2c6282398d736f43d5d7cf5ffa90
manojr123 <manojraghavan2008@yahoo.co.in>
2023-02-15 14:32:50 +0530

    Nested Population : Display comment and related user

---
+++ controllers/comments_controller.js
@@ -0,0 +1,28 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+
+module.exports.create = function(req, res) {
+    console.log('Comments Controller');
+
+    Post.findById(req.body.post, function (err, post) {
+
+        if ( post ) {
+            Comment.create({
+                content : req.body.content,
+                post : req.body.post,
+                user : req.user._id
+            }, function(err, comment){
+                //handle error
+                if (err)
+                    console.log('Error in Comments Controller');
+
+                post.comments.push(comment);
+                post.save();
+
+                res.redirect('/');
+            });
+        }
+    });
+
+}
