37af1fa8f8d4d6fde7990c288e889f1820ef5014
manojr123 <manojraghavan2008@yahoo.co.in>
2023-02-21 20:50:19 +0530

    Function to Send Mail

---
+++ config/nodemailer.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+let transporter = nodemailer.createTransport( {
+    // service : 'gmail',
+    // host : 'smtp.gmail.com',
+    // port : 587,
+    // secure : false,
+    // auth : {
+    //     user :'manojr124u@gmail.com',
+    //     pass : 'Gdev1234'
+    // }
+    service : 'mailtrap.io',
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "85bb51e1aef3de",
+      pass: "c53e93bc3c2102"
+    }
+  
+
+});
+
+let renderTemplate = (data,relativePath) => {
+    let mailHTML;
+    ejs.renderFile (
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template)   {
+            if (err) {console.log('error in rendering mailier template' ); return}
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+module.exports = {
+    transporter : transporter,
+    renderTemplate : renderTemplate
+}
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,8 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('./mailers/comments_mailer');
 
 module.exports.create = async function(req, res){
-
+console.log('inside comments controller:create')
     try{
         let post = await Post.findById(req.body.post);
 
@@ -15,7 +16,26 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+            // Similar for comments to fetch the user's id
+            comment =await comment.populate('user', 'name email');
 
+            // send email
+            commentsMailer.newComment(comment);
+            
+
+            // if (req.xhr) {
+            //     // Similar for comments to fetch the user's id
+            //     comment =await comment.populate('user', 'name').execPopulate();
+
+            //     return res.status(200).json({
+            //         data: {
+            //             comment : comment
+            //         },
+            //         message : 'Post created!'
+            //     })
+            // }
+
+            req.flash('success', 'Comment published!');
             res.redirect('/');
         }
     }catch(err){
